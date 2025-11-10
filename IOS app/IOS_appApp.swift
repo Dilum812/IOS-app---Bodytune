4bcfd203921559b8aebea569d3bf0945c4145310
dilumdissanayake <dilumthimiraz8@gmail.com>
2025-11-10 23:10:56 +0530

    Initial Commit

---
+++ IOS app/IOS_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_appApp.swift
+//  IOS app
+//
+//  Created by Dilum Dissanayake on 2025-11-10.
+//
+
+import SwiftUI
+
+@main
+struct IOS_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
